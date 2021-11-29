c8d6200a7886d2e6fd6707199962b3f0bfb8eb61
Phophrai <catwater1233@gmail.com>
2021-11-29 12:04:12 +0700

    LAB2

---
+++ LAB2/es6-array.js
@@ -0,0 +1,24 @@
+const cars = ["toyota", "honda", "mazda"]
+const fruits = []
+fruits[0] = "apple";
+fruits[3] = "mango"
+const rname = new array("win", "john", "jame")
+console.log(fruits[1]);
+
+const carpop = cars.pop();
+console.log(carpop);
+console.log(cars);
+
+cars.push("kububa");
+console.log(cars);
+
+const carsshift = cars.shift()
+console.log(carsshift);
+console.log(car);
+
+cars.unshift("mazda");
+console.log(cars);
+
+const slidearr = cars.slice(0,2);
+console.log(slidearr);
+
