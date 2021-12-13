ee57dc5358df5347511715ef70018e4589fe8e67
catwa <your_catwater1233@gmail.com>
2021-12-13 11:41:20 +0700

    commit

---
+++ LAB2/es6-object.js
@@ -0,0 +1,28 @@
+//es6-object.js
+let preson = {
+    name "Phophrai",
+    age:20,
+    eyecolor: "black",
+    function () => {
+        return `${preson name} : ${preson.age;}`;
+    },
+};
+
+const x = person;
+x age = 30;
+console.log(person.age);
+console.log(x.fullname());
+
+function addFn(...num) {
+    iet sum=0;
+    for(vale of num) sum += value;
+    return sum;     
+}
+console.log(addFu(10, 20, 30, 40));
+
+const [a, b] = [10,20,30];
+console.log(b);
+
+
+const {name: rName, age} = person;
+console.log(rName);
