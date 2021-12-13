cb7cbfb22e03bb7084d579caf65df7885eaf5ab7
catwa <your_catwater1233@gmail.com>
2021-12-13 19:31:50 +0700

    LAB3

--- LAB2/es6-object.js
+++ LAB2/es6-object.js
@@ -1,28 +1,28 @@
 //es6-object.js
 let preson = {
-    name "Phophrai",
+    name: "Phophrai",
     age:20,
     eyecolor: "black",
-    function () => {
-        return `${preson name} : ${preson.age;}`;
+    fullname: () => {
+        return `${preson.name} : ${preson.age}`
     },
 };
 
-const x = person;
-x age = 30;
-console.log(person.age);
+const x = preson;
+x.age = 30;
+console.log(preson.age);
 console.log(x.fullname());
 
 function addFn(...num) {
-    iet sum=0;
-    for(vale of num) sum += value;
+    let sum=0;
+    for(value of num) sum += value;
     return sum;     
 }
-console.log(addFu(10, 20, 30, 40));
+console.log(addFn(10, 20, 30, 40));
 
 const [a, b] = [10,20,30];
 console.log(b);
 
 
-const {name: rName, age} = person;
+const {name: rName, age} = preson;
 console.log(rName);
