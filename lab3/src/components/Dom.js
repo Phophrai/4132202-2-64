292daa8cced4d59c7cc04d83781413e95a2e5fbf
Jessada01 <630112418040@bru.ac.th>
2021-12-18 12:11:14 +0700

    LAB3

---
+++ lab3/src/components/Dom.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const age = 12;
+
+const dom = (
+  <>
+    <h1>Hello React !!!</h1>
+    <p>My name is Narongsak , {age} years old.</p>
+  </>
+);
+
+function Dom() {
+    return dom;
+}
+
+export default Dom;
